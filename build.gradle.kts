e70b62ec4c46539fc4740bef9f94691a390cc42f
Leonardo Colman Lopes <leonardo.colman98@gmail.com>
2019-04-08 19:54:52 -0300

    Implements publishing to maven central (#6)
    Resolves #3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
+import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.21"
+    `maven-publish`
+    signing
+    id("org.jetbrains.dokka") version "0.9.17"
+    
 }
 
-group = "com.kerooker.simplefeaturethrottle"
+group = "com.kerooker.simplefeaturethrottler"
 version = "1.0.0"
 
 repositories {
@@ -22,4 +27,72 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets.getByName("main").allSource)
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    val javadoc = tasks["dokka"] as DokkaTask
+    javadoc.outputFormat = "javadoc"
+    javadoc.outputDirectory = "$buildDir/javadoc"
+    dependsOn(javadoc)
+    classifier = "javadoc"
+    from(javadoc.outputDirectory)
+}
+
+publishing {
+    repositories {
+        
+        maven("https://oss.sonatype.org/service/local/staging/deploy/maven2") {
+            credentials {
+                username = System.getProperty("OSSRH_USERNAME")
+                password = System.getProperty("OSSRH_PASSWORD")
+            }
+        }
+    }
+    
+    publications {
+        
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar.get())
+            artifact(javadocJar.get())
+            
+            pom {
+                name.set("SimpleFeatureThrottler")
+                description.set("Simple Feature Throttler")
+                url.set("https://www.github.com/Kerooker/SimpleFeatureThrottler")
+                
+                
+                scm {
+                    connection.set("scm:git:http://www.github.com/Kerooker/SimpleFeatureThrottler/")
+                    developerConnection.set("scm:git:http://github.com/Kerooker/")
+                    url.set("https://www.github.com/Kerooker/SimpleFeatureThrottler")
+                }
+                
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                
+                developers {
+                    developer {
+                        id.set("Kerooker")
+                        name.set("Leonardo Colman Lopes")
+                        email.set("leonardo.colman98@gmail.com")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
 }
